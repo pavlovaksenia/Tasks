d41a766ae3ec87c53d1544f95aa8d05e1cf62d07
pavlovaksenia <pavksan@gmail.com>
2021-11-27 15:09:49 +0700

    Task003 Показать большее и меньшее число

---
+++ Task002/Program.cs
@@ -0,0 +1,10 @@
+﻿// Даны два числа. Показать большее и меньшее число
+Console.WriteLine("Введите первое число");
+double A = Convert.ToDouble (Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double B = Convert.ToDouble (Console.ReadLine());
+if (A>B)
+Console.WriteLine("Первое число - большее, второе - меньшее");
+else
+Console.WriteLine("Второе число - большее, первое - меньшее");
+
