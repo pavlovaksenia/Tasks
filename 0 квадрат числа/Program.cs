8c9d04e876c9d105e593cae3eeeee6cbd2256b23
pavlovaksenia <pavksan@gmail.com>
2021-11-27 14:33:25 +0700

    Task000

---
+++ 0 квадрат числа/Program.cs
@@ -0,0 +1,7 @@
+﻿// Найти квадрат числа
+Console.WriteLine("Введите число");
+double num = Convert.ToDouble (Console.ReadLine());
+double result = num*num;
+Console.WriteLine("Квадрат числа");
+Console.WriteLine(result);
+
