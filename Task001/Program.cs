b2d1bd401a77d0da8fce3cb73412e8f28fe77a76
pavlovaksenia <pavksan@gmail.com>
2021-11-27 15:01:00 +0700

    Task001 является ли а квадратом в

---
+++ Task001/Program.cs
@@ -0,0 +1,9 @@
+﻿// Является ли второе число квадратом первого
+Console.WriteLine("Введите первое число");
+double A = Convert.ToDouble (Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double B = Convert.ToDouble (Console.ReadLine());
+if (B==A*A)
+Console.WriteLine("Bторое число является квадратом первого");
+else
+Console.WriteLine("Bторое число не является квадратом первого");
