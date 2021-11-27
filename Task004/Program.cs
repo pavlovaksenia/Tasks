cad780387cf9eab7bb0c69070d5fcd9632a0051f
pavlovaksenia <pavksan@gmail.com>
2021-11-27 15:36:31 +0700

    Task004 Найти максимальное из трех чисел

---
+++ Task004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти максимальное из трех чисел
+Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int C = Convert.ToInt32 (Console.ReadLine());
+int max = A;
+if(B>max) max = B;
+if(C>max) max = C;
+Console.Write("Максимальное число ");
+Console.WriteLine (max);
