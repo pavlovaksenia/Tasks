44d166a5ffd6aedac16fbf885372cbdec467bea4
pavlovaksenia <pavksan@gmail.com>
2021-11-27 15:50:08 +0700

    Task005 Написать программу вычисления значения функции y = f(a)

---
+++ Task005/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+Console.WriteLine("Введите a");
+double a = Convert.ToDouble (Console.ReadLine());
+double function (double a)
+{
+    double f = a*3;
+    return f;
+}
+Console.Write("Функция от а равняется ");
+Console.WriteLine(function(a));
