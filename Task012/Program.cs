1b29c818f6c3c73fecd040d7ef9e4394adad64a6
pavlovaksenia <pavksan@gmail.com>
2021-12-03 23:50:35 +0700

    Task 13

---
+++ Task012/Program.cs
@@ -0,0 +1,11 @@
+﻿// Удалить вторую цифру трехзначного числа
+int numberRandom = new Random().Next(99,1000);
+Console.WriteLine($"Выбрано трехзначное число {numberRandom}");
+/*int numberRandom = Convert.ToInt32 (Console.ReadLine());
+Console. = numberRandom % 100/10;
+
+string MyString = $"{numberRandom}";
+Console.WriteLine(MyString.Remove(2));
+*/
+int mod = numberRandom % 100/10;
+Console.WriteLine ($"{numberRandom} remove")
---
+++ Task013/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+Console.WriteLine("Введите число A");
+int numberA = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32 (Console.ReadLine());
+if (numberA % numberB == 0)
+Console.WriteLine ("Число А кратно числу В");
+else
+{
+  Console.WriteLine ($"Остаток деления числа А на число В равен {numberA % numberB}");  
+}
