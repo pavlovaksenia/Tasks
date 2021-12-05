0b82d788bebe0fe057c7095415aa3b1c1c7b72b0
pavlovaksenia <pavksan@gmail.com>
2021-12-05 12:06:57 +0700

    Дано число. Проверить кратно ли оно 7 и 23

---
+++ Task014/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите любое число больше 100");
+int[] array = {};
+
+int n = array.Length; //определяет длину массива
+int find = 8;
+
+int index = 0;
+
+while(index<n)
+{
+if (array[index]==find)
+{
+    Console.WriteLine (index);
+    break;
+}
+    index++; //index = index+1;
---
+++ Task015/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+int numberRandom = new Random().Next(7,10000);
+Console.WriteLine($"Выбрано число {numberRandom}");
+if (numberRandom % 7 == 0)
+Console.WriteLine($" Число {numberRandom} кратно 7");
+else
+Console.WriteLine($" Число {numberRandom} не кратно 7");
+if (numberRandom % 23 == 0)
+Console.WriteLine($" Число {numberRandom} кратно 23");
+else
+Console.WriteLine($" Число {numberRandom} не кратно 23");
